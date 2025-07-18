87da3b108a286af3353c23ba67c79cbf9a89644c
Saidasignals <valitabdata@gmail.com>
2025-07-18 19:37:17 +0530

    Add files via upload
    two python files of beginner tasks

---
+++ Task-1.py
@@ -0,0 +1,15 @@
+# Task 1: Perform Basic Mathematical Operations
+#Problem Statement: Write a Python program that does the following:
+#1.  Takes two numbers as input from the user.
+#2.  Performs the basic mathematical operations on these two numbers:
+
+a = float(input("Enter a number: "))
+b = float(input("Enter another number: "))
+c = a + b
+print("Addition: ",c)
+d = a - b
+print("Subtraction: ",d)
+e = a * b
+print("Multiplication: ",e)
+f = a / b
+print("Division: ",f)
